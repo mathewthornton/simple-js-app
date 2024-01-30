e41966798c27ced85ea5544b0f6deedc57f24d12
Mat <mat.thornt@gmail.com>
2024-01-29 17:19:59 -0800

    created array of objects for pokemonList

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-alert('Hello World');
+let pokemonList = [
+    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+    {name: 'Slowking', height: 2, type: ['psychic', 'water']},
+    {name: 'Jynx', height: 1.4, type: ['psychic', 'ice']},
+    {name: 'Exeggutor', height: 2, type: ['psychic', 'grass']},
+    {name: 'Dragonite', height: 2.2, type: ['dragon', 'flying']},
+    {name: 'Gengar', height: 1.5, type: ['ghost', 'poison']}
+];
 
-let favoriteFood = "Lasagna";
-favoriteFood = "Shrimp Linguine"
-document.write(favoriteFood);
