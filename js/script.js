1c55fab5c33e1262aaba3c237215578c698c78a3
Mat <mat.thornt@gmail.com>
2024-02-17 16:11:41 -0800

    created IIFE to hold pokemonRepository

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
-let pokemonList = [
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-    {name: 'Slowking', height: 2, type: ['psychic', 'water']},
-    {name: 'Jynx', height: 1.4, type: ['psychic', 'ice']},
-    {name: 'Exeggutor', height: 2, type: ['psychic', 'grass']},
-    {name: 'Dragonite', height: 2.2, type: ['dragon', 'flying']},
-    {name: 'Gengar', height: 1.5, type: ['ghost', 'poison']}
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+        {name: 'Slowking', height: 2, type: ['psychic', 'water']},
+        {name: 'Jynx', height: 1.4, type: ['psychic', 'ice']},
+        {name: 'Exeggutor', height: 2, type: ['psychic', 'grass']},
+        {name: 'Dragonite', height: 2.2, type: ['dragon', 'flying']},
+        {name: 'Gengar', height: 1.5, type: ['ghost', 'poison']}];
 
-// for (let i = 0; i < pokemonList.length; i++){
-//     if (pokemonList[i].height > 2) {
-//         document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - WOAH that\'s big! </p>')
-//     } else {
-//         document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
-//     }
-// }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
-pokemonList.forEach(function(pokemon) {
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonRepository.add({name: 'Pikachu', height: 1.1, type: 'electric'});
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 2) {
          document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!</p>'); }
     else { document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') </p>'); }
 });
+
